84be9de7f77f014c7cb223ac9e782524bab5a1cf
MiguelSanLeon <>
2023-05-31 09:43:44 +0000

    Add variables and functions to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
 // Wait for the DOM to finish loading before running the game
+// Get DOM elements
+const startButton = document.getElementById('startButton');
+const playerButtons = document.getElementsByClassName('playerButtons');
+const computerCard = document.getElementById('computerCard');
+const playerCard = document.getElementById('playerCard');
+const resultText = document.getElementById('resultText');
+const computerWinsText = document.getElementById('computerWinsText');
+const playerWinsText = document.getElementById('playerWinsText');
+
+// Game variables
+let computerScore = 0;
+let playerScore = 0;
+
+// Game functions 
+/** Function start the game */
+function startGame() {
+
+}
+/** Function to play a round in the game */
+
+function playRound() {
+
+}
+/** Function to get a random choice fot the computer */
+
+function getRandomChoice() {
+
+}
+/** Function that determin the winner of a round */
+
+function roundWinner() {
+
+}
+/** Function to update score text */
+
+function updateScoreText() {
+
+}
+// Event listener
